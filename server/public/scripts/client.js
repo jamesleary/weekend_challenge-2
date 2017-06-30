80cdd46eb925c7c7b6bc9537d26945e39977fb4f
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-06-30 17:04:20 -0500

    added buttons to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,26 @@
 console.log('JS Sourced');
 $(document).ready(function(){
   console.log('jQuery Sourced');
+clickHandlers();
 });
+
+
+function clickHandlers (){
+  $('#add').on('click', function(){
+    console.log('add');
+  });
+  $('#subtract').on('click', function(){
+    console.log('subtract');
+  });
+  $('#divide').on('click', function(){
+    console.log('divide');
+  });
+  $('#multiply').on('click', function(){
+    console.log('multiply');
+  });
+  $('#calculate').on('click', function(){
+    console.log('calculate');
+  });
+
+
+}//end of clickHandlers
