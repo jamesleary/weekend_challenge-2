fdb0e21b814a1e301fdd3da3f7c17931a378207c
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-07-01 11:01:46 -0500

    added button functionality, if/else statement to the server logic, added POST and GET to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,70 @@
 console.log('JS Sourced');
+var input1;
+var input2;
+var operator;
 $(document).ready(function(){
   console.log('jQuery Sourced');
-clickHandlers();
+  clickHandlers();
+
 });
 
 
 function clickHandlers (){
   $('#add').on('click', function(){
     console.log('add');
+    $('#placeholder').text('+');
+    operator = 'add';
   });
   $('#subtract').on('click', function(){
     console.log('subtract');
+    $('#placeholder').text('-');
+    operator = 'subtract';
   });
   $('#divide').on('click', function(){
     console.log('divide');
+    $('#placeholder').text('/');
+    operator = ('divide');
   });
   $('#multiply').on('click', function(){
     console.log('multiply');
+    $('#placeholder').text('*');
+    operator = ('multiply');
   });
   $('#calculate').on('click', function(){
     console.log('calculate');
+    input1 = $('#variable1').val();
+    input2 = $('#variable2').val();
+
+    console.log(input1);
+    console.log(input2);
+    console.log(operator);
+    dataSend();
   });
-
-
 }//end of clickHandlers
+
+function dataSend (){
+  $.ajax({
+    type:"POST",
+    url:"/calculate",
+    data:{
+      input1:input1,
+      input2:input2,
+      operator:operator
+    },
+    success: function(response){
+      console.log('success POST');
+      dataReceive();
+    }
+  });
+}//end of dataSend
+
+function dataReceive(){
+  $.ajax({
+    type:"GET",
+    url:"/calculate",
+    success: function(response){
+      console.log(response);
+      
+    }
+  });
+}
--- server/server.js
+++ server/server.js
@@ -2,8 +2,33 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var port = 5000;
+var input1;
+var input2;
+var operator;
+var answer;
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/calculate', function(req, res){
+  var calculation = req.body;    //this is the DATA we set to data  on ajax post on client
+  console.log(calculation);
+  input1 = calculation.input1;
+  input2 = calculation.input2;
+  operator = calculation.operator;
+  logic();
+console.log(answer);
+  res.send({messege:'Successfully added our project!'});
+});
+
+ app.get('/calculation', function (){
+   console.log('sending calc');
+   answer = answer.toString();
+   res.send(answer);
+ });
+
+
+
 
 app.get('/*', function (req,res){
   var file = req.params[0] || 'views/index.html';
@@ -13,3 +38,18 @@ app.get('/*', function (req,res){
 app.listen(port, function(){
   console.log('Server is listening on port ', port);
 });
+
+function logic(){
+  if (operator == 'add') {
+    answer = parseInt(input1) + parseInt(input2);
+  }
+  else if (operator == 'subtract') {
+    answer =parseInt(input1) - parseInt(input2);
+  }
+  else if (operator == 'multiply') {
+    answer = parseInt(input1) * parseInt(input2);
+  }
+  else if (operator == 'divide') {
+    answer = parseInt(input1) / parseInt(input2);
+}
+}
