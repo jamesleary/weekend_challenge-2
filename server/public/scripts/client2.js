9e907795e50bbf646455774696fecee37b977808
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-07-01 14:00:20 -0500

    cleaned up some dry code and reorganized on client2.js, added more comments on functions to make it clear what each part was doing

--- server/public/scripts/client2.js
+++ server/public/scripts/client2.js
@@ -2,67 +2,69 @@ console.log('JS Sourced');
 var input1;
 var input2;
 var operator;
-
 $(document).ready(function(){
-
+  numberClick();
+  operatorSelect();
+  dataSend();
+  clearAll();
+});//end of document.ready
+//on click, put numbers into inputfield
+function numberClick(){
   $('.number').on('click', function(){
     $('.inputfield').text($('.inputfield').text()+ $(this).attr('id'));
-
     if (!operator) {
       input1 = $('.inputfield').text();
       console.log(input1);
     }
     else if (operator) {
-
       input2 = $('.inputfield').text();
     }
-
   });//end of number
-
+}
+//when an operator is clicked,empty the input field and set the operator to send
+function operatorSelect(){
   $('.operator').on('click',function(){
     $('.inputfield').empty();
     operator  = $(this).attr('id');
     console.log(operator);
   });//end of operator functionality
-
-  $('#calculate').on('click',function(){
-    console.log('====');
-    $.ajax({
-      type:"POST",
-      url:"/calculate",
-      data:{
-        input1:input1,
-        input2:input2,
-        operator:operator
-      },
-      success: function(response){
-        console.log('success POST');
-        dataReceive();
-      }
-    });
-  });//end of calculate
-  function dataReceive(){
-    $.ajax({
-      type:"GET",
-      url:"/calculate",
-      success: function(response){
-        console.log(response);
-        parseInt(response);
-        $('.inputfield').text(response);
-
-      }
-    });
-  }
-
+}
+//send variables to the server and send back the result as a number
+function dataSend(){$('#calculate').on('click',function(){
+  $.ajax({
+    type:"POST",
+    url:"/calculate",
+    data:{
+      input1:input1,
+      input2:input2,
+      operator:operator
+    },
+    success: function(response){
+      console.log('success POST');
+      dataReceive();
+    }
+  });
+});//end of calculate
+}
+//send back the answer with the given data
+function dataReceive(){
+  $.ajax({
+    type:"GET",
+    url:"/calculate",
+    success: function(response){
+      console.log(response);
+      parseInt(response);
+      $('.inputfield').text(response);
+    }
+  });
+}
+//clear button resets the calculator
+function clearAll(){
   $('.clear').on('click', function(){
     $('.inputfield').empty();
     input1 = 0;
     input2 = 0;
     operator = 0;
 
-  });
-
-
-
-
-});//end of document.ready
+  });//end of clear all
+}
